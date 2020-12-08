a3cb67473ae7d9c2a6b7879aa4dc1a8b7a2973ec
Christian Landgren <christian@landgren.nu>
2020-12-08 21:57:34 +0100

    second challenge day1 is complete

--- day1/index.js
+++ day1/index.js
@@ -1,6 +1,18 @@
 const input = require('./input.json')
 
-const sums = input.reduce((arr, a) => ([...arr, ...input.map(b => ({sum: a + b, product: a * b, a, b}))]), [])
-const result = sums.filter(nr => nr.sum === 2020)
+const twoSums = input
+  .reduce((arr, a) => ([...arr, ...input.map(b => ({sum: a + b, product: a * b, a, b}))]), [])
+  .filter(nr => nr.sum === 2020)
 
-console.log(sums, result)
+console.log(twoSums)
+
+const threeSums = input.reduce((arr, a) => (
+  [...arr, 
+    ...input.reduce((arr, b) => (
+      [...arr,
+        ...input.map(c => ({sum: a + b + c, product: a * b * c, a, b})).filter(nr => nr.sum === 2020)
+      ]
+    ), [])
+  ]), [])
+
+console.log(threeSums)
